ff8651668fd976aa884e14ba482f24af0319eda2
Mike Waud <mdwaud@gmail.com>
2020-01-03 14:42:30 -0500

    day 3 part 1

--- elixir/lib/advent_of_code/day_03.ex
+++ elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,54 @@
 defmodule AdventOfCode.Day03 do
-  def part1(args) do
+  defmodule WireTracer do
+    # returns a list of tuples for all the coordinates a wire crosses
+    def trace(trace_inputs) do
+      trace_inputs
+      |> Enum.reduce([{0,0}], fn(trace_input, acc) -> add_wire(trace_input, acc) end)
+    end
+
+    def add_wire({_direction, 0}, accumulator), do: accumulator
+    def add_wire({direction, distance}, accumulator) do
+      [current_coordinates | _] = accumulator # current position is the head of the list
+      new_coordinates = step(current_coordinates, direction)
+      add_wire({direction, distance - 1}, [new_coordinates | accumulator])
+    end
+
+    defp step({x,y}, "L"), do: {x-1,y}
+    defp step({x,y}, "R"), do: {x+1,y}
+    defp step({x,y}, "U"), do: {x,y-1}
+    defp step({x,y}, "D"), do: {x,y+1}
+  end
+
+  def part1(input) do
+    [input_1, input_2] = parse_input(input)
+
+    wire_1 = WireTracer.trace(input_1)
+    wire_2 = WireTracer.trace(input_2)
+
+    find_shortest_intersection(wire_1, wire_2)
   end
 
   def part2(args) do
+  end
+
+  defp parse_input(input) when is_binary(input) do
+    input
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(fn(s) ->
+      String.split(s, ",")
+      |> Enum.map(fn(ss) ->
+        <<direction::binary-1, distance_s::binary>> = ss
+        distance = String.to_integer(distance_s)
+        {direction, distance}
+      end)
+    end)
+  end
+
+  defp find_shortest_intersection(wire_1, wire_2) do
+    MapSet.intersection(MapSet.new(wire_1), MapSet.new(wire_2))
+    |> Enum.reject(fn(coord) -> coord == {0,0} end)
+    |> Enum.map(fn({x,y}) -> abs(x) + abs(y) end)
+    |> Enum.min
   end
 end
--- elixir/test/advent_of_code/day_03_test.exs
+++ elixir/test/advent_of_code/day_03_test.exs
@@ -2,13 +2,38 @@ defmodule AdventOfCode.Day03Test do
   use ExUnit.Case
 
   import AdventOfCode.Day03
+  alias AdventOfCode.Day03.WireTracer
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  describe "WireTracer#add_wire" do
+    test "simple case" do
+      assert [{1,0}, {0,0}] == WireTracer.add_wire({"R",1}, [{0,0}])
+    end
 
-    assert result
+    test "longer case" do
+      assert [{-3,0},{-2,0},{-1,0}, {0,0}] == WireTracer.add_wire({"L",3}, [{0,0}])
+    end
+  end
+
+  describe "WireTracer#trace" do
+    test "simple case" do
+      assert [{1,0}, {0,0}] == WireTracer.trace([{"R",1}])
+    end
+
+    test "longer case" do
+      assert [{1,2},{1,1},{1,0}, {0,0}] == WireTracer.trace([{"R",1}, {"D",2}])
+    end
+  end
+
+  describe "part 1" do
+    test "example 1" do
+      input =
+"R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83"
+
+      result = part1(input)
+
+      assert 159 == result
+    end
   end
 
   @tag :skip
